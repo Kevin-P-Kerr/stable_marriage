5e559cfd13ff216be220d61c62dc37a6568f64a4
Kevin <kkerr87@gmail.com>
2015-06-20 17:26:38 -0400

    more progress

--- stable_marriage.py
+++ stable_marriage.py
@@ -4,7 +4,7 @@ def doStableMarriage(proposers,proposees):
       proposer.propose()
     for proposee in proposees
       proposee.rejectCandidates()
-  return MarriageCard(proposees)
+  printMarriage(proposees)
 
 class Proposer:
   def __init__(self, preferences):
@@ -15,16 +15,50 @@ class Proposer:
   def propose():
     for potentialSpouse in self.preferences:
       if (!potentialSpouse.has_rejected):
-        potentialSpouse.proposee.sendProposal()
+        potentialSpouse.proposee.sendProposal(self)
         return
-  def sendRejection(proposeeName):
-    self.proposeeLookUpDict[name].has_rejected = True
+  def sendRejection(proposee):
+    for potentialSpouse in preferences
+      if (proposee == potentialSpouse.proposee):
+        potentialSpouse.has_rejected = True
+        return
 
 class PotentialSpouse:
   def __init__(self,proposee,name)
     self.proposee = proposee
-    self.name = name
+    self.has_rejected = False
+
+class Proposee:
+  def __init__(self,preferences):
+    self.currentlyAccepted = False
+    self.perferences = perferences
+    self.tentativelyAccepted = []
+  def sendProposal(proposee):
+    self.tenativelyAccepted.append(proposee)
+  def rejectCandidates():
+    if (!tentativelyAccepted.length):
+      return
+    if (!self.currentlyAccepted):
+      self.currentlyAccepted = tentativelyAccepted[0]
+    for proposer in tentativelyAccepted:
+      if (preferences.index(proposer) < preferences.index(self.currentlyAccepted):
+        self.currentlyAccepted = proposer
+      else:
+        proposer.sendRejection(self)
 
 
+def unstable(proposers,proposees):
+  
+
+
+def printMarriage(proposees)
+  for p in proposees:
+    print p.currentlyAccepted
+
+
+
+          
+
+     
   
  
