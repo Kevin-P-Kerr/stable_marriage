471026d1376f85fa6f7c796880b106372a37e9ed
Kevin <kkerr87@gmail.com>
2015-06-20 16:51:49 -0400

    add a proposer class

--- stable_marriage.py
+++ stable_marriage.py
@@ -6,4 +6,18 @@ def doStableMarriage(proposers,proposees):
       proposee.rejectCandidates()
   return MarriageCard(proposees)
 
+class Proposer:
+  def __init__(self, preferences):
+    self.perferences = preferences
+    self.proposeeLookUpDict = {}
+    for potentialSpouse in preferences:
+      self.proposeeLookUpDict[potentialSpouse.name] = potentialSpouse
+  def propose():
+    for potentialSpouse in self.preferences:
+      if (!potentialSpouse.has_rejected):
+        potentialSpouse.proposee.sendProposal()
+        return
+  def sendRejection(proposeeName):
+    self.proposeeLookUpDict[name].has_rejected = True
   
+ 
